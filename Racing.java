34d222568b8920a622b326880ec5a13809a51b2b
Caleb Aragones <caleb.aragones@calbaptist.edu>
2024-01-18 20:40:11 -0800

    Implemented skeleton menu bar and quit game option

--- Racing.java
+++ Racing.java
@@ -1,31 +1,16 @@
 package PACKAGE_NAME;
-import java.util.Random;
+import java.awt.*;
 
-import java.time.LocalTime;
-import java.time.temporal.ChronoUnit;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JButton;
-import javax.swing.JComponent;
-import javax.swing.KeyStroke;
-import javax.swing.AbstractAction;
-import javax.swing.JComboBox;
+import javax.swing.*;
 
 import javax.imageio.ImageIO;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-
-import java.awt.geom.AffineTransform;
-import java.awt.image.AffineTransformOp;
 import java.util.Vector;
 
 public class Racing { // incorporating audio, starting traffic light, start menu with car selection
@@ -42,7 +27,8 @@ public class Racing { // incorporating audio, starting traffic light, start menu
         pi = 3.14159265358979;
         twoPi = 2.0 * 3.14159265358979;
         endgame = false;
-        level = 1;
+
+
         //lap count
         //best time 2 dec
         // p1Speed 0 dec
@@ -52,7 +38,13 @@ public class Racing { // incorporating audio, starting traffic light, start menu
         // implement initial player locations
 
         try {
-            File background = ImageIO.read(new File ("track.png"))
+            background = ImageIO.read(new File ("track.png"));
+            supra = ImageIO.read(new File("supra.png"));
+            porche = ImageIO.read(new File("porche.png"));
+            brz = ImageIO.read(new File("brz.png"));
+            avSupra = ImageIO.read(new File("avSupra.png"));
+            avPorche = ImageIO.read(new File("avPorche.png"));
+            avBrz = ImageIO.read(new File("avBrz.png"));
         }
         catch (IOException e) {
 
@@ -60,7 +52,7 @@ public class Racing { // incorporating audio, starting traffic light, start menu
     }
 
     private static void backgroundDraw(){
-        Graphics g = appFrame.getGraphics();
+
     }
 
     private static class ImageObject{
@@ -75,7 +67,6 @@ public class Racing { // incorporating audio, starting traffic light, start menu
         public ImageObject(){
 
         }
-
 
         public ImageObject(double xinput, double yinput, double xwidthinput,
                            double yheightinput, double angleinput){
@@ -129,13 +120,87 @@ public class Racing { // incorporating audio, starting traffic light, start menu
         }
     }
 
+    private static class StartGame{
+    }
+
+    private static class PlayerMove{
+
+    }
+
+    private static class ConfirmQuit implements ActionListener {
+        private ConfirmQuit(){
+
+        }
+
+        public void popup(){
+            int quit = JOptionPane.showConfirmDialog(null, "Are you sure you want to quit?",
+                    "Confirm Quit",
+                    JOptionPane.YES_NO_OPTION);
+
+            if (quit == 0) System.exit(0);
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            popup();
+        }
+    }
+
+    private static class SelectCar implements ActionListener{
+        private SelectCar(){
+
+        }
+
+        public void openMenu(){
+
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+
     public static void main(String[] args) {
         setup();
+        launch();
+    }
+
+    private static void launch(){
+        appFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        appFrame.setSize(1280, 720);
+
+        JPanel menuBar = new JPanel();
+        menuBar.setBackground(Color.GREEN);
+        menuBar.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 5));
+
+        JButton startGameButton = new JButton("Start Game");
+        menuBar.add(startGameButton);
+
+        JButton carSelectButton = new JButton("Select Car");
+        menuBar.add(carSelectButton);
+
+        JButton exitButton = new JButton("Quit Game");
+        menuBar.add(exitButton);
+        exitButton.addActionListener(new ConfirmQuit());
+
+        appFrame.add(menuBar, BorderLayout.SOUTH);
+
+        appFrame.setLocationRelativeTo(null);
+        appFrame.setVisible(true);
     }
 
     private static boolean endgame;
     private static BufferedImage background;
+    private static BufferedImage supra;
+    private static BufferedImage porche;
+    private static BufferedImage brz;
+    private static BufferedImage avSupra;
+    private static BufferedImage avPorche;
+    private static BufferedImage avBrz;
     private static BufferedImage player1;
+    private static BufferedImage player2;
 
     private static boolean upPressed;
     private static boolean downPressed;
